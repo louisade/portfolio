151da9ab5105e0d6c3d1c6aabcc29d21b350ebb9
louisade <50155001+louisade@users.noreply.github.com>
2019-05-19 19:55:26 +0200

    working js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 jQuery(document).ready(function () {
-    $('div.hidden').hide(0).delay(500).fadeIn(3000);
+    $('div.hidden').hide(0).delay(0).fadeIn(500);
+
+    $('.link').click(function(event) {
+		event.preventDefault();
+		newLocation = this.href;
+
+	$('div.hidden').fadeOut(500, newpage);
+
+	});
+
+function newpage() {
+
+window.location = newLocation;
+
+}
+
 		});
